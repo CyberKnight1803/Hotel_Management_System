4b0e74863a70721309b3925adb9bef1104a3e044
Siddhesh-Shukla <siddheshshukla1024@gmail.com>
2021-04-07 09:49:37 +0530

    Authentication modified

--- customer_home/views.py
+++ customer_home/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 def home(request):
-    return render(request, 'customer_home/base.html')
+    return render(request, 'customer_home/customer_home.html')
