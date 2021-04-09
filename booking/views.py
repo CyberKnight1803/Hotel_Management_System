deff2576763060a38050fcb93a41a0b553b8c710
CyberKnight1803 <omkarpitale2001@gmail.com>
2021-04-09 23:27:59 +0530

    Room avialability algorithm implemented

--- booking/views.py
+++ booking/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import BookingForm
 from django.contrib import messages
 from .models import Reservation
-from .pdf import renderPDF
-
+from .utils import renderPDF, isAvailable
 from django.core.mail import EmailMessage
 from django.conf import settings
 from django.template.loader import render_to_string
@@ -25,31 +24,34 @@ def bookings(request):
             roomtype = form.cleaned_data['roomtype']
             rooms = form.cleaned_data['rooms']
 
-            booking = Reservation(checkin=checkin, checkout=checkout, adults=adults, children=children,
-            roomtype=roomtype,rooms=rooms, email=user_email)
-            booking.save()
-            time = booking.date_requested
-            B = Reservation.objects.raw('SELECT "reservationID" FROM booking_reservation WHERE "email"=%s and "date_requested"=%s', [user_email, time])
-            details = {
-                'name': username,
-                'checkin': checkin,
-                'checkout': checkout,
-                'roomtype': roomtype,
-                'rooms': rooms,
-            }
-            pdf = renderPDF('booking/booking_pdf.html', details, 'booking.pdf')
-            template = render_to_string('booking/email.html', details)
-            email = EmailMessage(
-                'Reservation Confirmation #' + str(B[0].reservationID), 
-                template,
-                settings.EMAIL_HOST_USER,
-                [user_email]
-            )
-            email.fail_silently=False
-            email.attach_file('booking.pdf', pdf)
-            email.send()
-            messages.success(request, f'Booking Queued')
-            return redirect('Customer_Home')
+            booking = Reservation(checkin=checkin, checkout=checkout, adults=adults, 
+            children=children, roomtype=roomtype,rooms=rooms, email=user_email)
+            if isAvailable(booking):
+                booking.save()
+                time = booking.date_requested
+                B = Reservation.objects.raw('SELECT "reservationID" FROM booking_reservation WHERE "email"=%s and "date_requested"=%s', [user_email, time])
+                details = {
+                    'name': username,
+                    'checkin': checkin,
+                    'checkout': checkout,
+                    'roomtype': roomtype,
+                    'rooms': rooms,
+                }
+                pdf = renderPDF('booking/booking_pdf.html', details, 'booking.pdf')
+                template = render_to_string('booking/email.html', details)
+                email = EmailMessage(
+                    'Reservation Confirmation #' + str(B[0].reservationID), 
+                    template,
+                    settings.EMAIL_HOST_USER,
+                    [user_email]
+                )
+                email.fail_silently=False
+                email.attach_file('booking.pdf', pdf)
+                email.send()
+                messages.success(request, f'Booking Queued')
+                return redirect('Customer_Home')
+            else:
+                form = BookingForm()
     else:
         form = BookingForm()
 
