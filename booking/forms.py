528fc519cc3c30db86d3867f29a72376a0200a4f
CyberKnight1803 <omkarpitale2001@gmail.com>
2021-04-14 01:07:37 +0530

    Working on availability

--- booking/forms.py
+++ booking/forms.py
@@ -5,3 +5,12 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model = Reservation
         fields = ['checkin', 'checkout', 'adults', 'children', 'roomtype', 'rooms']
+
+    checkin = forms.DateField(
+        widget=forms.DateInput(format='%d-%m-%Y'),
+        input_formats=('%d/%m/%Y', )
+    )
+    checkout = forms.DateField(
+        widget=forms.DateInput(format='%d-%m-%Y'),
+        input_formats=('%d-%m-%Y', )
+    )
---
+++ staff_home/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.conf import settings
+
+class DateInput(forms.DateInput):
+    input_type = 'date'
+
+class AvailabilityForm(forms.Form):
+    date = forms.DateField(widget=DateInput, input_formats=['%Y-%m-%d'])
+                               
