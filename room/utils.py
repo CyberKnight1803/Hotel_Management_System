92379913128300de91b4605ceda0a6d05c2097b7
CyberKnight1803 <omkarpitale2001@gmail.com>
2021-04-17 02:13:52 +0530

    Added backend live room status checkin and checkout logics

--- room/utils.py
+++ room/utils.py
@@ -1,10 +1,6 @@
 from booking.models import Reservation
 from .models import Room
-
-def CheckIN(form_input):
-
-    query = 'SELECT * FROM room_room WHERE '
-
+from django.db import connection
 
 def availableRooms(Date):
     R = {'Single' : 1, 'Double' : 2, 'Suite' : 3, 'Luxury' : 4}
@@ -20,8 +16,46 @@ def availableRooms(Date):
         'suite' : 5 - len(suite),
         'luxury' : 5 - len(luxury),
     }
-    
     return Availability
 
-def insertRoom(reservationID):
-    pass
+def checkInRoom(reservationID, roomNo):
+    ROOM = {
+        1 : 'SINGLE',
+        2 : 'DOUBLE',
+        3 : 'SUITE',
+        4 : 'LUXURY',
+    }
+    c = connection.cursor()
+    booking = Reservation.objects.raw('SELECT * FROM booking_reservation WHERE "reservationID"=%s', [reservationID])
+    room_type = ROOM[booking.roomtype]
+    try:
+        c.execute('UPDATE room_room SET "reservationID_id" = %s, "status" = %s WHERE "room_no" = %s', [reservationID, 'OCCUPIED', roomNo])
+        c.execute('UPDATE booking_reservation SET "checkin_status"= %s WHERE "reservationID" = %s', [True, reservationID])
+    finally:
+        c.close()
+
+def checkOutRoom(room_no):
+    c = connection.cursor()
+    room_details = Room.objects.raw('SELECT * FROM room_room WHERE "room_no" = %s', [room_no])
+    reservationID = room_details.reservationID
+    
+    try:
+        c.execute('UPDATE room_room SET "reservationID_id" = %s, "status" = %s WHERE "room_no" = %s', [None, 'VACANT',room_no])
+        c.execute('UPDATE booking_reservation SET "checkout_status"= %s WHERE "reservationID" = %s', [True, reservationID])
+
+    finally:
+        c.close()
+    
+    return reservationID
+
+
+def liveRoomStats():
+    query = 'SELECT * FROM room_room WHERE "status" = %s'
+    rooms = Room.objects.raw('SELECT * FROM room_room')
+    room_status = rooms[:]['status']
+    liveStats = [1 if status == 'OCCUPIED' else 0 for status in room_status]
+    occ_rooms = Room.objects.raw(query, ['OCCUPIED'])
+    vac_rooms = Room.objects.raw(query, ['VACANT'])
+
+    return liveStats
+
