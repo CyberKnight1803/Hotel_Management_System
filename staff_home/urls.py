daca41f6d4e6a0d7ad9e2a937bc102a8a37d3142
Siddhesh-Shukla <siddheshshukla1024@gmail.com>
2021-04-19 18:01:03 +0530

    Links Added

--- staff_home/urls.py
+++ staff_home/urls.py
@@ -5,7 +5,7 @@ from booking import views as booking_views
 urlpatterns = [
     path('', views.home, name='home'),
     path('detail/<int:pk>/', views.detail.as_view(), name='detailview'),
-    path('rooms/', booking_views.grid_view),
-    path('availability/', views.availability),
+    path('rooms/', booking_views.grid_view, name='rooms'),
+    path('availability/', views.availability, name='availability'),
     # path('popup_available/', views.popup_available),
 ]
